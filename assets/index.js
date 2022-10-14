b081ccc20fa7059c74ac818928b98fd67e736a50
BrownCAB <brown.calah@hotmail.com>
2022-10-13 22:43:40 -0700

    Modified Index js

--- assets/index.js
+++ assets/index.js
@@ -1,19 +1,8 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
-
-
-// prompted for information about my application repository
+//const questions = [];
+// Prompted for information about my application repository
 const inquirer = require('inquirer')
 const fs = require('fs')
 
@@ -39,6 +28,8 @@ inquirer.prompt([
 	name:'',
 	message:'',
 }
+// TODO: Create a function to write README file
+//function writeToFile(fileName, data) {}
 ]).then(function(answers) {
     const readMeData = inputAnswers(answers)
     fs.writeFile('README.md', readMeData, (error) => {
@@ -48,7 +39,7 @@ inquirer.prompt([
 
 const inputAnswers = (answers) => {
     console.log(answers)
-    // On line 38 input reademe structure
+    //Input reademe structure
     return `
     # Title 
     //project title
@@ -73,3 +64,9 @@ const inputAnswers = (answers) => {
     //email address and how to reach me
  `
 }
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
