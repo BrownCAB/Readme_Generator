dcf8e50179db6c9db7da58edb8a3292c59c26a70
BrownCAB <110314270+BrownCAB@users.noreply.github.com>
2022-10-14 17:38:53 -0700

    Modified Index JS

--- assets/index.js
+++ assets/index.js
@@ -1,52 +1,68 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs').promises;
 
 // TODO: Create an array of questions for user input
 //const questions = [];
-// Prompted for information about my application repository
-const inquirer = require('inquirer')
-const fs = require('fs')
 
-inquirer.prompt([
-    {
-	type:'input',
-	name:'yourName',
-	message: 'What is your name?',
-}, {
-    type:'input',
-	name:'',
-	message:'',
-}, {
-    type:'input',
-	name:'',
-	message:'',
-}, {
-    type:'input',
-	name:'',
-	message:'',
-}, {
-    type:'input',
-	name:'',
-	message:'',
-}
+const questions = [] => {
+    return inquirer.prompt([
+         {
+        	type:'input',
+        	name:'title',
+        	message: 'What would you like to title your project?',
+        }, {
+        type:'input',
+        	name:'desc',
+        	message:'Describe your project?',
+        }, {
+        type:'input',
+        	name:'install',
+        	message:'What are the steps required to install your project?',
+        }, {
+        type:'input',
+        	name:'usage',
+        	message:'How do you use this application?',
+        }, {
+        type:'input',
+        	name:'contributes',
+        	message:'List your collaborators.',
+        }, {
+        type:'input',
+        	name:'tests',
+        	message:'',
+        }, {
+        type:'input',
+        	name:'license',
+        	message:'Choose a license.',
+        	liscence: ['ISC', 'MIT', 'Apache', 'BSD3', 'BSD2', 'GPL/LGPL', 'Mozilla', 'CDD'],
+        }, {
+        type:'input',
+        	name:'github',
+        	message:'What is your Github Username?',
+        }, {
+        type:'input',
+        	name:'email',
+        	message:'What is your email address?',
+        },  
+    ]);
+};
+
 // TODO: Create a function to write README file
 //function writeToFile(fileName, data) {}
-]).then(function(answers) {
-    const readMeData = inputAnswers(answers)
-    fs.writeFile('README.md', readMeData, (error) => {
-        error ? console.log(error) : console.log('Success!')
-    }) 
-});
-
-const inputAnswers = (answers) => {
-    console.log(answers)
+function writeTofile(title, decs, install) => {
     //Input reademe structure
-    return `
-    # Title 
+    `
+    # ${title} 
     //project title
     ## Description
     //
     ## Table of Contents
-    //Clickable links 
+        Title
+        Installation
+        Usage
+        Contributing
+        Tests
 
     ## Installation
 
@@ -61,12 +77,15 @@ const inputAnswers = (answers) => {
     ## Questions
     //GitHub username
     //GitHub Link
-    //email address and how to reach me
- `
-}
+    //email address and how to reach me'`
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {} => {
+    promptUser()
+    .then((answers) => writeFile('Readme.md', generateReadMe(answers)))
+    .then(() => console.log('Successfully added to Readme'))
+    .catch((err) => console.error(err));
 
 // Function call to initialize app
 init();
